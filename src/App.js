28cb39734b8583e6cc05adaec9ee48dca574744c
Vikst009 <96147918+Vikst009@users.noreply.github.com>
2025-04-12 09:14:01 +0200

    clear all list items

--- src/App.js
+++ src/App.js
@@ -19,6 +19,13 @@ export default function App() {
     );
   }
 
+  function clearItems() {
+    const confirmed = window.confirm(
+      "Are you sure you want to delete all items?"
+    );
+    if (confirmed) setItems([]);
+  }
+
   return (
     <div className="app">
       <Logo />
@@ -27,6 +34,7 @@ export default function App() {
         items={items}
         onDeleteItem={handleDeleteItem}
         onPackedItem={handleToggle}
+        onClearItems={clearItems}
       />
       <Statistics items={items} />
     </div>
@@ -81,11 +89,24 @@ function Form({ onAddItems }) {
   );
 }
 
-function PackingList({ items, onDeleteItem, onPackedItem }) {
+function PackingList({ items, onDeleteItem, onPackedItem, onClearItems }) {
+  const [sortBy, setSortBy] = useState("input");
+  let sortedItems;
+
+  if (sortBy === "input") sortedItems = items;
+  if (sortBy === "description")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => a.description.localeCompare(b.description));
+  if (sortBy === "packed")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => Number(a.packed) - Number(b.packed));
+
   return (
     <div className="list">
       <ul>
-        {items.map((item) => (
+        {sortedItems.map((item) => (
           <Item
             item={item}
             key={item.id}
@@ -94,6 +115,18 @@ function PackingList({ items, onDeleteItem, onPackedItem }) {
           />
         ))}
       </ul>
+      <div>
+        <select
+          className="actions"
+          value={sortBy}
+          onChange={(e) => setSortBy(e.target.value)}
+        >
+          <option value="input">Sort by input</option>
+          <option value="description">Sort by description</option>
+          <option value="packed">Sort by packed status</option>
+        </select>
+        <button onClick={onClearItems}>Clear list</button>
+      </div>
     </div>
   );
 }
